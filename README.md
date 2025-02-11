360f790649e2a10a1f07a9b3d2273e3a9cc9a60b
Sibur <benjamin@boudry.org>
2025-02-11 23:23:50 +0100

    Innit commit suite à un problème

--- README.md
+++ README.md
@@ -4,16 +4,35 @@
 
 ### Pré-requis:
 - Docker 
+- Meson
 
 ### Compilation 
+
+#### Avec Docker
 ```
 docker build -t battle_bot -f Dockerfile .
 ```
 
+#### Sans Docker
+```
+meson setup builddir
+meson compile -C builddir
+```
+
+
 ### Lancement 
+
+#### Avec Docker
 ``` 
 docker run battle_bot
 ```
+
+#### Sans Docker
+
+``` 
+./builddir/battle_c_bot
+```
+
 
 ## Scoring
 
@@ -21,3 +40,53 @@ docker run battle_bot
 - Détruire un ennemi: 50 points
 - Récupérer un boost: 30 points
 
+
+
+## Fonctionnalités du Bot
+
+### Déplacement Automatique :
+``` 
+Le bot se dirige automatiquement vers le boost le plus proche pour maximiser ses capacités.
+Évitement d'Obstacles :
+
+Si un obstacle est détecté sur le chemin vers le boost, le bot change de direction pour l'éviter.
+```
+
+### Détection via le Radar :
+``` 
+Le bot utilise un radar pour identifier les objets autour de lui, tels que :
+```
+- Ennemis : Priorité à leur élimination.
+- Ressources (boosts) : Pour augmenter ses capacités.
+- Obstacles : Pour les contourner.
+
+### Gestion des Actions :
+``` 
+Le bot décide de ses actions en fonction des données du radar :
+```
+- Tirer sur un ennemi.
+- Changer de direction pour éviter un obstacle.
+- Se diriger vers une ressource pour la récupérer.
+
+
+### Stratégie Dynamique :
+``` 
+Le bot ajuste constamment sa trajectoire et ses actions en fonction de l'évolution de l'arène.
+```
+
+## Fichiers du projet
+
+### Fichiers Sources dans src
+
+- main.c 
+- radar.c
+- deplacement.c
+- tir.c
+
+
+### Fichiers d'En-tête dans le include 
+
+- deplacement.h 
+- main.h 
+- radar.h 
+- tir.h 
--- src/main.c
+++ src/main.c
@@ -1,25 +1,91 @@
-#include "battle_c.h"
-
+#include "radar.h"
+#include "tir.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "unistd.h"
+#include "deplacement.h"
+#include <pthread.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    // Établissement de la connexion avec le serveur
+    BC_Connection *conn = bc_connect("5.135.136.236", 8080);
+    printf("Connexion réussie\n");
+    fflush(stdout);
 
-  BC_Connection *conn = bc_connect("5.135.136.236", 8080);
+    // Récupération des informations de la carte
+    BC_WorldInfo info_monde = bc_get_world_info(conn);
+    printf("Dimensions de l'arène\nX: %ld, Y: %ld\n", info_monde.map_x, info_monde.map_y);
+    fflush(stdout);
 
-  bc_get_world_info(conn);
+    // Initialisation des données du joueur
+    BC_PlayerData data = bc_get_player_data(conn);
+    fflush(stdout);
 
-  bc_set_speed(conn, 1.2, 0.4, 0);
+    // Initialisation des listes pour le radar
+    Radar *joueurs = NULL;    // Liste des joueurs détectés
+    Radar *murs = NULL;       // Liste des murs détectés
+    Radar *boosts = NULL;     // Liste des boosts détectés
 
-  BC_PlayerData data = bc_get_player_data(conn);
+    // Initialisation des données du bot
+    int health = 100;
+    int score = 0;
 
-  BC_List *list = bc_radar_ping(conn);
+    // Initialisation de la vitesse du joueur
+    float speed = 20.0;
 
-  do {
-    BC_MapObject *map_object = (BC_MapObject *)bc_ll_value(list);
-    printf("map_object x = %d, y = %d", map_object->position.x,
-           map_object->position.y);
+    while (!data.is_dead)
+    {
+        fflush(stdout);
 
-  } while (((list = bc_ll_next(list)) != NULL));
-  return EXIT_SUCCESS;
+        // Mise à jour des listes d'objets détectés par le radar
+        scan(conn, info_monde, data, &joueurs, &murs, &boosts);
+
+        // Gestion du déplacement vers le boost le plus proche
+        BC_MapObject boost_proche = proximite(boosts, data);
+        if (boost_proche.position.x < 1 && boost_proche.position.y < 1) // Vérification de l'existence d'un boost
+        {
+            fflush(stdout);
+            // Déplacement aléatoire si aucun boost n'est trouvé
+            deplacement(conn, rand() % info_monde.map_x, rand() % info_monde.map_y, data, &speed);
+        }
+        else
+        {
+            fflush(stdout);
+            deplacement(conn, boost_proche.position.x, boost_proche.position.y, data, &speed);
+        }
+
+        // Gestion du tir sur les joueurs
+        if (joueurs == NULL)
+        {
+            fflush(stdout);
+        }
+        else
+        {
+            BC_MapObject joueur_proche = proximite(joueurs, data);
+            fflush(stdout);
+            // Tir dirigé vers le joueur
+            Tir(conn, data.position.x, joueur_proche.position.x, data.position.y, joueur_proche.position.y);
+        }
+        
+        BC_PlayerData bot = bc_get_player_data(conn);
+        if (bot.health < health) {
+            printf("Vie : %d\n", bot.health);
+            health = bot.health;
+        }
+        if (bot.score > score) {
+            printf("score : %d\n", bot.score);
+            score = bot.score;
+        }
+    }
+
+    // Nettoyage mémoire et déconnexion
+    free_list(joueurs);
+    free_list(murs);
+    free_list(boosts);
+
+    bc_disconnect(conn);
+
+    printf("Fin du script\n");
+    return EXIT_SUCCESS;
 }
